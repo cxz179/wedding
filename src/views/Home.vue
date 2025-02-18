29cd41e5d00355ef273bafe983b85d93d6302a66
cxz179 <zxcvbnml317@163.com>
2025-02-19 00:35:53 +0800

    增加头部完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,25 @@
         <div class="layer2"></div>
         <div class="layer3"></div>
         <div class="info-box">
-            sass星空
+            <div class="top-row font-25">
+                <div class="arco-col">
+                    💖
+                    <span>挚爱至上</span>
+                    💖
+                </div>
+                <div class="arco-col font-20 last-text">
+                    💕
+                    <span>遇见你,就像跋山涉水遇见一轮月亮</span>
+                    💕
+                </div>
+            </div>
+            <div class="container">
+                <div class="card1"></div>
+                <div class="card2"></div>
+                <div class="card3">
+                    * Card stacks are awesome and inspired by real card stack at your table.
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -17,6 +35,7 @@
 @use "sass:meta"; // 引入 sass:meta 模块
 @use "sass:math"; // 引入 math 模块
 @use "sass:string";
+@use "@/assets/common.scss" as *;
 
 @function getShadows($n) {
     $shadows: '#{math.random(100)}vw #{math.random(100)}vh #fff';
@@ -24,6 +43,7 @@
     @for $i from 2 through $n {
         $shadows: '#{$shadows}, #{math.random(100)}vw #{math.random(100)}vh #fff';
     }
+
     @return string.unquote($shadows);
 }
 
@@ -32,11 +52,13 @@
     height: 100vh;
     overflow: hidden;
     background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
-    $duration:400s;
-    $count:1000;
+    $duration: 400s;
+    $count: 1000;
+
     @for $i from 1 through 3 {
         $duration: math.floor(math.div($duration, 2));
-        $count: math.floor(math.div($count,2));
+        $count: math.floor(math.div($count, 2));
+
         .layer#{$i} {
             $size: #{$i}px;
             position: fixed;
@@ -61,11 +83,52 @@
             }
         }
     }
-    .info-box{
-        width: 60%;
-        
-        background: #fff;
+
+    .info-box {
+        width: 80%;
+        color: #fff;
         margin: 0 auto;
+        .top-row{
+            width: 100%;
+            padding: 20px 0;
+            @include flex-box('row','space-between','center');
+            .arco-col{
+                flex: 1;
+            }
+            .last-text{
+                text-align: end;
+            }
+        }
+        .container {
+            position: relative;
+            margin-top: 60px;
+            width: 360px;
+
+            >* {
+                width: 340px;
+                height: 120px;
+                border: solid 1px #bebebe;
+                background-color: #1f1f1f;
+                position: absolute;
+                border-radius: 10px;
+                padding: 10px;
+                color: #fff;
+                box-shadow: 0px 8px 20px -10px #bbbbbb;
+                text-shadow: 0px 0px 5px #fff;
+                letter-spacing: 1px;
+                background-image: radial-gradient(circle 160px at 50% 120%, #353535, #1f1f1f);
+            }
+
+            .card1 {
+                width: 300px;
+                margin: -20px 0px 0px 20px;
+            }
+
+            .card2 {
+                width: 320px;
+                margin: -10px 0px 0px 10px;
+            }
+        }
     }
 }
 
